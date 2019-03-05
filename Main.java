f1960280fad694cde6646070581d6d2ec2f291e8
Justin-Fernbaugh <fernbaughj@gmail.com>
2019-03-05 14:51:37 -0800

    Main changed.

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+
 class Main 
 {
+
+    public Calculations calculate = new Calculations();
+    public ArrayList<String> applications = new ArrayList<String>();
+
     public static void main(String[] args) {
-        Calculations calculate = new Calculations();
 
 
     }
@@ -9,6 +14,11 @@ class Main
     public void startupEntries()
     {
         System.out.println("Choose which app you'd like to use.");
+        for(int i = 0; i < applications.size(); i++)
+        {
+            System.out.println(String.format("%s. %s", i, applications.get(i)));
+        }
+        System.out.println("");
     }
 
 }
