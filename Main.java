45f6177ce3a47ce237e3df6d505c682976a9253e
Justin-Fernbaugh <fernbaughj@gmail.com>
2019-03-06 14:33:14 -0800

    Visual Studio stuff.

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Main 
 {
 
     public Calculations calculate = new Calculations();
     public ArrayList<String> applications = new ArrayList<String>();
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
@@ -18,6 +20,12 @@ class Main
         {
             System.out.println(String.format("%s. %s", i, applications.get(i)));
         }
+
+        for(int i = 0; i < applications.size(); i++)
+        {
+            //if statement to check if input equals 
+        }
+
         System.out.println("");
     }
 
