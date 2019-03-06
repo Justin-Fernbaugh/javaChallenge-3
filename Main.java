3971c72688171281762bed8d9639435530ed0a99
Justin-Fernbaugh <fernbaughj@gmail.com>
2019-03-06 14:41:10 -0800

    Main check what user inputed.

--- Main.java
+++ Main.java
@@ -20,10 +20,15 @@ class Main
         {
             System.out.println(String.format("%s. %s", i, applications.get(i)));
         }
+        String userInput = scanner.nextLine();
 
         for(int i = 0; i < applications.size(); i++)
         {
             //if statement to check if input equals 
+            if(userInput.equals(applications.get(i).toString()));
+            {
+                //check what was selected and then use that app.
+            }
         }
 
         System.out.println("");
